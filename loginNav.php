c16f91f90b8924ecd12a78927883a201c08ff439
RashmiNirasha <2020is043@stu.ucsc.cmb.ac.lk>
2022-02-16 16:48:05 +0530

    sorted out login and components

--- loginNav.php
+++ loginNav.php
@@ -1,17 +1,61 @@
 <html>
-
 <head>
-    <link href="/assets/css/topnav.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="assets\css\test.css">
+<link rel="stylesheet" href="assets\css\background.css">
+<link rel="stylesheet" href="assets\css\form.css">
 </head>
-
-
 <body>
-    <div class="topnav">
+    <nav class="navigation-bar">
+        <img class="logo" src="assets\img\logo.png" width="76px" height="57px">
+        <p style="margin-left: 20px;">Fixetics</p>
+        <a href="support.php">Help</a>
+        <a href="#about">Log In</a>  
+    </nav>
+    <div class="bg">
+    <div class="center">
+            <div class="bodycontent">
+                <div class="form" >
+                    <form method="GET" action="">
+            
+                        <h2>LOGIN</h2>
+    
+                        <div class="formcontent">
+                            <div class="formlabel"> User Name: </div>
+                            <div class="formin"><input class="input_box" type="text" name="username" required></div>
+                        </div>
+            
+                        <br>
+    
+                        <div class="formcontent">
+                            <div class="formlabel"> Password: </div>
+                            <div class="formin"><input class="input_box" type="password" name="password" required></div>
+                        </div>
+            
+                        <br>
+    
+                        <div class="formremember">
+                            <div class="formlabel"><input type="checkbox">Remember &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Forgot Password?</div>
+                        </div>
+    
+                        <div class="formcontent">
+                            <div class="formin" >
+                                <a href="#"><button class="button" type="Submit">  Sign in </button> </a>
+                            </div>
+                        </div>
+    
+                        <br>
+    
+                        <div class="formlabel">New User? <a class="createlink" href="">Create an Account</a></div>
+                        <p style="text-align: right;">
+                            Haven't registered yet?<b>
+                                <a href="admin/customerForm.php">Sign Up</a></b></br>
 
-        <a href="index.php" class="navright">LOG IN</a>
-        <a href="help.php" class="navright">Help</a>
-
+                        
+                    </form>
+                    <a href="adminlog.php" style="float: right;"><b>Log in as admin</b></a>
+                </div>
+            </div>  
+        </div>
     </div>
-</body>
-
+    </body>
 </html>
